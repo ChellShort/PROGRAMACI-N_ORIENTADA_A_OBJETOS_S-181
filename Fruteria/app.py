232635f808c4138e9778f10bc0151685826ec789
PROFILE <121040103@upq.edu.mx>
2023-07-07 16:46:14 -0600

    Fruteria 2° iteración

--- Fruteria/app.py
+++ Fruteria/app.py
@@ -41,10 +41,34 @@ def guardar(): #metodo guardar
     flash('El album fue agregado correctamente')
     return redirect(url_for('index'))
 
+@app.route('/editar/<string:id>')
+def editar(id): #metodo editar
+    cursorID= mysql.connection.cursor()
+    cursorID.execute('SELECT * from tbfrutas where id=%s', (id,))
+    consulID=cursorID.fetchone()
+    return render_template('editarFruta.html', fruta=consulID)
 
-@app.route('/eliminar')
-def eliminar(): #metodo eliminar
-    return "Se elimino la base de datos"
+@app.route('/actualizar/<id>', methods=['POST'])
+def actualizar(id): #metodo actualizar
+    if request.method == 'POST':
+        varFruta=request.form['txtFruta']
+        varTemporada=request.form['txtTemporada']
+        varPrecio=request.form['txtAnio']
+        varStock=request.form['txtAnio']
+
+        curAct = mysql.connection.cursor() # objeto de tipo cursor
+        curAct.execute('UPDATE tbfrutas set fruta = %s, temporada= %s, precio =%s, stock=%s WHERE id=%s',(varFruta, varTemporada, varPrecio, varStock, id))
+        mysql.connection.commit()
+    flash('La fruta ' + varFruta + ' fue actualizada correctamente ')
+    return redirect(url_for('index'))
+
+@app.route('/borrar/<id>')
+def borrar(id):
+    cursorBorrar=mysql.connection.cursor()
+    cursorBorrar.execute('DELETE from tbfrutas WHERE id=%s', (id,))
+    mysql.connection.commit()
+    flash('La fruta fue eliminado')
+    return redirect(url_for('index'))
 
 #ejecución del servidor en el puerto 5000
 if __name__ == '__main__':
